b987d4cde7054a4ca78bcbf6fd1b7aeb7b979708
phamngochau1506@gmail.com <haupham156>
2019-10-23 14:33:49 +0700

    Spring_JPA_Customer_Management_Tree_paging

--- src/main/java/com/codegym/cms/service/CustomerService.java
+++ src/main/java/com/codegym/cms/service/CustomerService.java
@@ -2,9 +2,11 @@ package com.codegym.cms.service;
 
 import com.codegym.cms.model.Customer;
 import com.codegym.cms.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomerService {
-    Iterable<Customer> findAll();
+    Page<Customer> findAll(Pageable pageable);
 
     Customer findById(Long id);
 
@@ -14,5 +16,5 @@ public interface CustomerService {
 
     Iterable<Customer> findByProvince(Province province);
 
-//    Iterable<Customer> findAllByProvince(Province province);
+    Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
 }
